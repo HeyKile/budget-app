e451565c75cfadb46d371bb491c6bd31f7a82c0f
HeyKile <bellokp2002@gmail.com>
2024-05-12 11:35:59 -0400

    auth middleware?

--- budget_app/src/main.rs
+++ budget_app/src/main.rs
@@ -2,11 +2,12 @@ use budget_app::{
     config::{*}, config::{*}, establish_connection, routes::{auth_routes::*, category_routes::*, overage_routes::*, purchase_routes::*}, services::auth_service::validate_token, utils::auth_utils::get_token
 };
 use axum::{
-    extract::Request, http::{HeaderMap, StatusCode}, middleware::{self, Next}, response::{IntoResponse, Response}, routing::{delete, get, post}, Extension, Json, Router
+    body::Body, extract::Request, http::{HeaderMap, StatusCode}, middleware::{self, Next}, response::{IntoResponse, Response}, routing::{delete, get, post}, Extension, Json, Router
 };
 use diesel::SqliteConnection;
 use dotenvy::dotenv;
 use serde_json::json;
+use tower_sessions_sqlx_store::sqlx::query;
 use std::{env, sync::{Arc, Mutex}};
 use tower::{ServiceBuilder, ServiceExt, Service};
 use http::{header, request, Method};
@@ -34,11 +35,9 @@ fn init_router(conn: AppState) -> Router {
         .allow_methods([Method::GET, Method::POST, Method::DELETE])
         .allow_headers(vec![header::CONTENT_TYPE])
         .allow_origin(Any); // unsafe, but fine for now...
-    Router::new()
-        .route("/", get(root))
-        .route("/login", post(login_handler))
+    
+    let auth_routes = Router::new()
         .route("/logout", post(logout_handler))
-        .route("/register", post(register_handler))
         .route("/categories/", get(get_all_categories_handler))
         .route("/categories/", post(post_category_handler))
         .route("/categories/:req_id", get(get_category_handler))
@@ -49,24 +48,37 @@ fn init_router(conn: AppState) -> Router {
         .route("/purchases/:req_id", delete(delete_purchase_handler))
         .route("/overages", post(overages_handler))
         .route("/users", get(get_users_handler))
+        .layer(ServiceBuilder::new().layer(middleware::from_fn(auth_middleware))); // TODO: fix to work
+
+    Router::new()
+        .route("/", get(root))
+        .route("/login", post(login_handler))
+        .route("/register", post(register_handler))
+        .nest("/auth", auth_routes)
         .layer(
             ServiceBuilder::new()
+                .layer(Extension(conn.clone()))
                 .layer(cors)
-                // .layer(auth) // TODO: fix to work, figure out how to pass values to next layer
-                .layer(Extension(conn))
         )
 }
 
-// async fn auth(headers: HeaderMap, request: Request, next: Next) -> impl IntoResponse {
-//     match get_token(&headers) {
-//         Some(token) if validate_token(token) => {
-//             let response = next.run(request).await;
-//             Ok(response)
-//         },
-//         _ => Err(StatusCode::UNAUTHORIZED)
-//     }
-// }
+async fn auth_middleware(request: Request, next: Next) -> Response {
+    let headers = request.headers();
+    let token = get_token(&headers);
+    let is_valid = match token {
+        None => false,
+        Some(token) => validate_token(token)
+    };
 
+    if is_valid {
+        next.run(request).await
+    } else {
+        Response::builder()
+            .status(StatusCode::UNAUTHORIZED)
+            .body(Body::from("unauthorized"))
+            .unwrap()
+    }
+}
 
 
 async fn root() -> impl IntoResponse {
