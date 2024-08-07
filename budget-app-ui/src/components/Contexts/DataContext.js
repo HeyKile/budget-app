1747dd0d9d0f642f33d4ee78d80b87ea9c79944e
HeyKile <bellokp2002@gmail.com>
2024-08-07 15:14:59 -0400

    Added TODO

--- budget-app-ui/src/components/Contexts/DataContext.js
+++ budget-app-ui/src/components/Contexts/DataContext.js
@@ -5,6 +5,7 @@ import { getPurchases } from "../../utils/PurchaseUtils";
 
 export const DataContext = createContext();
 
+// TODO: find some way to updates purchases and categories when added w/o calling api again
 export function DataProvider({ children }) {
     const token = useContext(UserTokenContext);
     const [categories, setCategories] = useState([]);
