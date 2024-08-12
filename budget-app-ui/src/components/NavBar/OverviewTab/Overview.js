caf74ab0703fbdf9a1f6006889c209a3a166a3ad
HeyKile <bellokp2002@gmail.com>
2024-08-12 17:54:44 -0400

    clean-up

--- budget-app-ui/src/components/NavBar/OverviewTab/Overview.js
+++ budget-app-ui/src/components/NavBar/OverviewTab/Overview.js
@@ -9,56 +9,8 @@ function Overview() {
     const [showPurchaseCreator, setShowPurchaseCreator] = useState(false);
     const { categories, setCategories } = useContext(DataContext);
     const { purchases, setPurchases } = useContext(DataContext);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        // if (categories.length === 0) {
-        //     const fetchCategories = fetch("http://localhost:5000/budget-app/api/category/get", {
-        //         method: "GET",
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             'Origin': 'http://localhost:3000',
-        //             "Authorization": `Bearer ${token}`
-        //         },
-        //     })
-        //     .then(response => {
-        //         if (!response.ok) {
-        //             throw new Error(`Network error`);
-        //         }
-        //         return response.json();
-        //     })
-        //     .then(data => {
-        //         setCategories(data.categories);
-        //     });
-
-        //     const fetchPurchases = fetch("http://localhost:5000/budget-app/api/purchase/get", {
-        //         method: "GET",
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             'Origin': 'http://localhost:3000',
-        //             "Authorization": `Bearer ${token}`
-        //         },
-        //     })
-        //     .then(response => {
-        //         if (!response.ok) {
-        //             throw new Error("Error retrieving data");
-        //         }
-        //         return response.json();
-        //     })
-        //     .then(data => {
-        //         setPurchases(data.purchases);
-        //     });
-
-        //     Promise.all([fetchCategories, fetchPurchases])
-        //     .then(() => {
-        //         setLoading(false);
-        //     })
-        //     .catch(error => {
-        //         console.error(error);
-        //         setError(error);
-        //         setLoading(false);
-        //     });
         if (categories.length === 0 || purchases.length === 0) {
             setLoading(true);
         } else {
